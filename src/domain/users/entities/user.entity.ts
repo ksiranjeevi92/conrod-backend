e10ff358b50f27baaf97847ab761288614fb06bf
siranjeevi <ksiranjeevi92@gmail.com>
2024-11-05 11:57:00 +0530

    mapping user entity

--- src/domain/users/entities/user.entity.ts
+++ src/domain/users/entities/user.entity.ts
@@ -1,7 +1,18 @@
+import { RegistryDates } from 'common/embedded/registry-dates.embedded';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class User {
+  @PrimaryGeneratedColumn()
   id: string;
+  @Column()
   name: string;
+  @Column({ unique: true })
   email: string;
+  @Column()
   password: string;
+  @Column({ unique: true })
   phone: string;
+  @Column(() => RegistryDates, { prefix: false })
+  registryDates: RegistryDates;
 }
