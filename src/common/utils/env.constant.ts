0523bf9fd821f97e518ec3b1a392f7c778c74c00
siranjeevi <ksiranjeevi92@gmail.com>
2025-03-23 19:18:07 +0530

    Upgraded to nestjs version 11

--- src/common/utils/env.constant.ts
+++ src/common/utils/env.constant.ts
@@ -1,6 +1,7 @@
 import * as Joi from 'joi';
 
 export const ENV_VALIDATION_SCHEMA = Joi.object({
+  APP_PORT: Joi.number().required(),
   DATASOURCE_USERNAME: Joi.required(),
   DATASOURCE_PASSWORD: Joi.required(),
   DATASOURCE_HOST: Joi.required(),
